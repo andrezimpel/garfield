2cca0e6218920e1487942b8eb80afcacf1678fb5
Andre Zimpel <andre@unknownbasics.com>
2017-08-01 13:54:53 +0200

    added full <picture> support, v0.1.2
    Signed-off-by: Andre Zimpel <andre@unknownbasics.com>

--- src/js/garfield.js
+++ src/js/garfield.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Garfield: garfield.js 0.1.1
+ * Garfield: garfield.js 0.1.2
  * http://www.github.com/andrezimpel/garfield
  * ========================================================================
  * Copyright 2017 Andre Zimpel
@@ -14,19 +14,19 @@
   // GARFIELD CLASS DEFINITION
   // ======================
 
-  var Garfield = $.garfield = function (element, options) {
+  var Garfield = function (element, options) {
     this.options             = options
     this.$body               = $(document.body)
     this.$element            = $(element)
     this.loaded              = false
   }
 
-  Garfield.VERSION  = '0.1.1'
+  Garfield.VERSION  = '0.1.2'
 
   Garfield.DEFAULTS = {
     'offsets': {
-      top: 200,
-      bottom: 200
+      top: 400,
+      bottom: 400
     },
     dataSrcSetAttribute: 'data-srcset',
     dataSrcAttribute: 'data-src',
@@ -35,15 +35,20 @@
   }
 
   Garfield.prototype.prepare = function () {
-    var options = this.options
-    var $element = this.$element
-    var that = this
+    var options       = this.options
+    var $element      = this.$element
+    var $observer     = $element
+    var that          = this
+    var $parent       = $element.parent()[0]
 
-    // remove garfield class
-    $element.removeClass(options.unloadedCssClass)
+    // check if the parent is within the picture element - this would prevent scrollMonitor from detecting propperly
+    if ($parent.tagName == 'PICTURE') {
+      $observer = $parent
+    }
+
 
     // install watcher
-    var elementWatcher = scrollMonitor.create($element, this.options.offsets);
+    var elementWatcher = scrollMonitor.create($observer, this.options.offsets);
 
     elementWatcher.enterViewport(function() {
       if (that.loaded === false) {
@@ -60,7 +65,6 @@
 
     $element.attr('src', src)
     $element.attr('srcset', srcSet)
-    $element.addClass(options.loadedCssClass)
 
     this.loaded = true
   }
